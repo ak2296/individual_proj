754c9f42fbe9cda27c0cfad263772d48c6efb60f
ak2296 <mohamad.gholinia@gmail.com>
2021-12-10 15:24:01 +0100

    12

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['elohin_m@yahoo.com']
+    ADMINS = ['mohamad.gholinia@gmail.com']
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     POSTS_PER_PAGE = 10
     BOOTSTRAP_BTN_STYLE = 'outline-primary rounded-pill'
